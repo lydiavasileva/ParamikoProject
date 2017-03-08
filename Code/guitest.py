a2394545da6727991574fc8538394b521432038a
Lydia Vasileva <lydiavasileva@users.noreply.github.com>
2017-03-08 23:26:36 +0100

    made default values work

--- Code/guitest.py
+++ Code/guitest.py
@@ -6,74 +6,63 @@ class ShowInfoGui:
 		#Main window
 		self.main_window = tkinter.Tk()
 
-		#Creating the two frames
+		#Set Window Default Size
+		self.main_window.geometry('400x300')
+
+		#Set Window Title
+		self.main_window.wm_title("File Transfer Program")
+
+		#Creating the three frames
 		self.top_frame = tkinter.Frame(self.main_window)
 		self.middle_frame = tkinter.Frame(self.main_window)
 		self.bottom_frame = tkinter.Frame(self.main_window)
 
-
-		#Variables
+		#Variables that are user input
 		self.ipAddr = tkinter.StringVar()
 		self.usrN = tkinter.StringVar()
 		self.usrP = tkinter.StringVar()
+		self.destPath= tkinter.StringVar()
+		self.sourPath = tkinter.StringVar()
 
+		#Setting HeaderFrame with a HeaderLabel
+		self.head_label = tkinter.Label(self.top_frame, text="File Transfer Program", font=(16))
+		self.head_label.pack(side = "top", fill = "both")
 
-		#Sets the frame where the info is shown
-		self.head_label = tkinter.Label(self.top_frame, \
-									text="Initial Connection") 
-		self.head_label.pack(side = "left")
-		
+		#Setting labels and entry widget for the different user inputs
+		#Setting default entries
+		self.ipAddr_label = tkinter.Label(self.middle_frame, text="IP Adress:").grid(row=0,column=0)
 
-		self.ipAddr_label = tkinter.Label(self.middle_frame, \
-									text="Ip Adress:").grid(row=0,column=0)
+		self.ipAddr_entry = tkinter.Entry(self.middle_frame, bd =5)
+		self.ipAddr_entry.grid(row=0, column=1)
+		self.ipAddr_entry.insert(0, "10.0.0.0")
 
-		self.ipAddr_entry = tkinter.Entry(self.middle_frame, \
-									bd =5).grid(row=0,column=1)
+		self.usrN_label = tkinter.Label(self.middle_frame, text="Username:").grid(row=1, column=0)
 
+		self.usrN_entry = tkinter.Entry(self.middle_frame, bd =5).grid(row=1, column=1)
 
+		self.usrP_label = tkinter.Label(self.middle_frame, text="Password:").grid(row=2,column=0)
+		self.usrP_entry = tkinter.Entry(self.middle_frame, bd =5).grid(row=2,column=1)
 
-		self.usrN_label = tkinter.Label(self.middle_frame, \
-									text="Username:").grid(row=1,column=0)
+		self.destPath_label = tkinter.Label(self.middle_frame, text="Destination Path:").grid(row=3,column=0)
+		self.destPath_entry = tkinter.Entry(self.middle_frame, bd =5).grid(row=3,column=1)
 
-		self.usrN_entry = tkinter.Entry(self.middle_frame, \
-									bd =5).grid(row=1,column=1)
+		self.sourPath_label = tkinter.Label(self.middle_frame, text="Source Path:").grid(row=4,column=0)
+		self.sourPath_entry = tkinter.Entry(self.middle_frame, bd =5).grid(row=4,column=1)
 
+		#Sets the button widgets
+		#NEED TO ADD: FUNCTIONS FOR HELP AND CONNECT
+		self.get_button = tkinter.Button(self.bottom_frame, text="Get", command=self.info).grid(row=0,column=0, pady=50)
+		self.get_button = tkinter.Button(self.bottom_frame, text="Push", command=self.info).grid(row=0,column=1, pady=50)
+		self.help_button = tkinter.Button(self.bottom_frame, text="Help", command=self.info).grid(row=0,column=2, pady=50)
+		self.quit_button = tkinter.Button(self.bottom_frame, text="Quit", command=self.main_window.destroy).grid(row=0,column=3, pady=50)
 
-		self.usrP_label = tkinter.Label(self.middle_frame, \
-									text="Password:").grid(row=2,column=0)
-		
-
-		self.usrP_entry = tkinter.Entry(self.middle_frame, \
-									bd =5).grid(row=2,column=1)
-		
-
-
-
-		#Sets the frames for the buttons
-		self.showInfo_button = tkinter.Button(self.bottom_frame, \
-											text="Connect", \
-											command=self.info)
-
-		self.help_button = tkinter.Button(self.bottom_frame, \
-										text="Help", \
-										command=self.info)
-		self.quit_button = tkinter.Button(self.bottom_frame, \
-										text="Quit", \
-										command=self.main_window.destroy)
-
-		self.showInfo_button.pack(side="left")
-		self.help_button.pack(side="left")
-		self.quit_button.pack(side="left")
-
-
-
+		#Packing the frames
 		self.top_frame.pack()
 		self.middle_frame.pack()
 		self.bottom_frame.pack()
 
-		# Enter the tkinter main loop.
+		#Enter the tkinter main loop.
 		tkinter.mainloop()
-
 
 	# The info method is a callback function for
 	# the show info button.
@@ -81,7 +70,7 @@ class ShowInfoGui:
 		name = "Kenneth Helweg Hansen"
 		address = "Ridderhatten 93B"
 		city = "Odense SØ, 5220"
-		
+
 		self.name.set(name)
 		self.addr.set(address)
 		self.city.set(city)
