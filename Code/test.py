4b619d405f62cc9cdbce883b876c88250e4b29df
Helweg <kenneth@helweghansen.dk>
2017-03-08 13:05:24 +0100

    Made SCP work

--- Code/test.py
+++ Code/test.py
@@ -1,14 +1,15 @@
+import paramiko
 from paramiko import SSHClient
 from scp import SCPClient
 
 ssh = SSHClient()
 ssh.load_system_host_keys()
-ssh.connect('example.com')
-
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname='10.210.14.132', port=22, username="root", password="juniper1")
 # SCPCLient takes a paramiko transport as its only argument
 scp = SCPClient(ssh.get_transport())
 
-scp.put('test.txt', 'test2.txt')
-scp.get('test2.txt')
+#scp.get('/config/juniper.conf.1.gz', '/home/helweg/Dokumenter/ParamikoProject/conf1.1.gz')
+scp.put('/home/helweg/Dokumenter/ParamikoProject/conf1.1.gz', '/config/juniper.conf.test.gz')
 
 scp.close()
