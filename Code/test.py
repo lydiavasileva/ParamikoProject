e4aa53fa2814b01632f14d837d77a57c5fec12fc
Helweg <kenneth@helweghansen.dk>
2017-03-08 14:24:54 +0100

    Made the connection part into a class

--- Code/test.py
+++ Code/test.py
@@ -2,14 +2,30 @@ import paramiko
 from paramiko import SSHClient
 from scp import SCPClient
 
-ssh = SSHClient()
-ssh.load_system_host_keys()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(hostname='10.210.14.132', port=22, username="root", password="juniper1")
-# SCPCLient takes a paramiko transport as its only argument
-scp = SCPClient(ssh.get_transport())
+class SSHConnection:
+	"""docstring for SCPConnection"""
+	def __init__(self, ip, uName, uPass):
+		self.ssh = SSHClient()
+		self.ssh.load_system_host_keys()
+		self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		self.ssh.connect(hostname=ip, port=22, username=uName, password=uPass)
 
-#scp.get('/config/juniper.conf.1.gz', '/home/helweg/Dokumenter/ParamikoProject/conf1.1.gz')
-scp.put('/home/helweg/Dokumenter/ParamikoProject/conf1.1.gz', '/config/juniper.conf.test.gz')
+	def getFile(self, srcDir, dstDir):
+		self.scp = SCPClient(self.ssh.get_transport())
 
-scp.close()
+		self.scp.get(srcDir, dstDir)
+
+		self.scp.close()
+
+	def pushFile(self, srcDir, dstDir):
+		self.scp = SCPClient(self.ssh.get_transport())
+
+		self.scp.put(srcDir, dstDir)
+
+		self.scp.close()
+
+myConn = SSHConnection('10.210.14.132', 'root', 'juniper1')
+
+#myConn.getFile('/config/juniper.conf.1.gz','/home/helweg/Dokumenter/ParamikoProject/conf1.test.gz')
+
+myConn.pushFile('/home/helweg/Dokumenter/ParamikoProject/conf1.test.gz','/config/juniper.conf.test.gz')
